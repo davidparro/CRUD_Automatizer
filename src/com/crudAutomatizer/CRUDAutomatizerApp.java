7bf8e0fe9f2111c313d20d36286cd6261fe78df3
David Parro Rubio <david.parro@alten.es>
2020-03-25 15:10:34 +0100

    Updated form

--- src/com/crudAutomatizer/CRUDAutomatizerApp.java
+++ src/com/crudAutomatizer/CRUDAutomatizerApp.java
@@ -3,8 +3,13 @@ package com.crudAutomatizer;
 import javax.swing.*;
 
 public class CRUDAutomatizerApp {
-    private JButton Aceptar;
     private JPanel containerPane;
+    private JLabel PathLabel;
+    private JLabel ClassLabel;
+    private JLabel TestLabel;
+    private JButton button1;
+    private JButton button2;
+    private JCheckBox checkBox1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
